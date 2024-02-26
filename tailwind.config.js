31a5255f9dc425b42419cfbb25fe6e3c9228552a
henry-jacq <henryeditz267@gmail.com>
2024-02-26 18:27:45 +0530

    change: custom colors and fonts added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@
 module.exports = {
     content: ["./src/**/*.{html,js}", "./index.html", "./src/input.css"],
     theme: {
-        extend: {},
+        fontFamily: {
+            montserrat: ['Montserrat', 'sans-serif'],
+            inter: ['Inter', 'sans-serif'],
+            primary: ['Open Sans', 'sans-serif']
+        },
+        extend: {
+            colors: {
+                'primary': '#0872BF',
+                'background': '#141A1A'
+            },
+        },
     },
     plugins: [],
 }
